208c7c0af3ca148d04518b6ab5a8aeab2cfaedf9
Philippe Timothe <PhilippeTimothe@gmail.com>
2021-03-21 15:30:12 -0400

    Array lesson complete

--- 10-all-playgrounds-for-part-2/begin/Part 2 - (ALL EPISODES) Beginning Collections - Begin.playground/Pages/05 - Operating on Arrays.xcplaygroundpage/Contents.swift
+++ 10-all-playgrounds-for-part-2/begin/Part 2 - (ALL EPISODES) Beginning Collections - Begin.playground/Pages/05 - Operating on Arrays.xcplaygroundpage/Contents.swift
@@ -3,6 +3,35 @@
 
 var pastries: [String] = ["cookie", "danish", "cupcake", "donut", "pie", "brownie"]
 
+let firstThree = Array(pastries[1...3])
+firstThree[0]
+
+pastries.append("KrispyKreme")
+//pastries.removeAll()
+
+pastries.isEmpty
+pastries.count
+
+// Optional binding for pastries.first
+if let first = pastries.first {
+  print(first)
+}
+
+//pastries.last
+
+pastries.contains("KrispyKreme")
+
+pastries.insert("Dunkin Donut", at: 3)
+let removeAt = pastries.remove(at: 3)
+removeAt
+//pastries.removeLast()
+
+// Change multiple elements at once.
+pastries[0...1] = ["apple fritter", "oldfashion donut"]
+pastries
+
+pastries.swapAt(0, 1)
+
 
 
 //: [⇒ Next: 06 - Challenge - Arrays](@next)
