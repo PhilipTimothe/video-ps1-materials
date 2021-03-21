1e0793094e5e6989bc5bf42f5b443ae8b93c268f
Philippe Timothe <PhilippeTimothe@gmail.com>
2021-03-21 15:09:33 -0400

    Challenge Complete

--- 12-challenge-tuples/begin/Part 2 - Episode 3 - Challenge - Tuples - Begin.playground/Contents.swift
+++ 12-challenge-tuples/begin/Part 2 - Episode 3 - Challenge - Tuples - Begin.playground/Contents.swift
@@ -8,7 +8,7 @@
  */
 
 // TODO: Write solution here
-
+let specialDate: (Int, Int, Int, String) = (03, 21, 2021, "Vacation")
 
 /*:
  Create another tuple, but this time name the constituent components. Give them names related to the data that they contain: month, day, year and description.
@@ -16,15 +16,18 @@
 
 
 // TODO: Write solution here
-
+let specialDay = (month: 03, day: 21, year: 2021, description: "Vacation")
 /*:
  In one line, read the day and description values into two constants. You’ll need to use the underscore to ignore the month and year.
  */
 
 // TODO: Write solution here
-
-
+let (_, day, _, description) = specialDay
+day
+description
 /*:
  Up until now, you’ve only seen constant tuples. But you can create variable tuples, too. Create one more tuple, like in the exercises above, but this time use var instead of let. Now change the day to a new value. */
 
 // TODO: Write solution here
+var iphone = (month: 03, day: 21, year: 2021, name: "iphone day")
+iphone.name = "Nope not really!"
