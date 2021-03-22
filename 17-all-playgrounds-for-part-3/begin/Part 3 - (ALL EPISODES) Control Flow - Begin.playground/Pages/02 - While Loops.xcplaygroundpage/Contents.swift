31cbca766f0acdaba02b9254495a6b1b1943972b
Philippe Timothe <PhilippeTimothe@gmail.com>
2021-03-22 13:59:20 -0400

    Lesson and challenge completed.

--- 17-all-playgrounds-for-part-3/begin/Part 3 - (ALL EPISODES) Control Flow - Begin.playground/Pages/02 - While Loops.xcplaygroundpage/Contents.swift
+++ 17-all-playgrounds-for-part-3/begin/Part 3 - (ALL EPISODES) Control Flow - Begin.playground/Pages/02 - While Loops.xcplaygroundpage/Contents.swift
@@ -1,5 +1,25 @@
 //: [⇐ Previous: 01 - Introduction](@previous)
 //: ## Episode 02: While Loops
+/*
+ while <#condition#> {
+  loop <#code#>
+ }
+*/
+// While Loop checks the condition before execution.
 
+var i = 1
 
+while i < 10 {
+  print(i)
+  i += 1
+}
+
+print("Counting Up Again!")
+
+// Repeat While Loop checks the condition after execution.
+i = 1
+repeat {
+  print(i)
+  i += 1
+} while i < 9
 //: [⇒ Next: 03 - Challenge - While Loops](@next)
--- 17-all-playgrounds-for-part-3/begin/Part 3 - (ALL EPISODES) Control Flow - Begin.playground/Pages/03 - Challenge - While Loops.xcplaygroundpage/Contents.swift
+++ 17-all-playgrounds-for-part-3/begin/Part 3 - (ALL EPISODES) Control Flow - Begin.playground/Pages/03 - Challenge - While Loops.xcplaygroundpage/Contents.swift
@@ -14,7 +14,12 @@
  
  Explain why the final value of `count` and the value in the last line printed out are different.
  */
+var count = 1
 
+while count <= 9 {
+  print("Counting up: \(count)")
+  count += 1
+}
 
 
 
@@ -31,10 +36,10 @@
  What are three different loop conditions you could use with this loop, and get the same result?
  
  */
-
-
-
-
+repeat {
+  print("Counting down: \(count)")
+  count -= 1
+} while count >= 0
 
 /*:
  
@@ -56,8 +61,14 @@
  
  Set the loop condition so that the repeat while loop finishes when the first 6 is rolled.
  */
+var rollCount = 0
+var roll = 0
 
-
+repeat {
+  roll = Int.random(in: 1...6)
+  rollCount += 1
+  print("Roll \(rollCount) gives you a \(roll)")
+} while roll != 6
 
 
 
