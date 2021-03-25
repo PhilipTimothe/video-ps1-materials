64da3da6419a5bdd246388d81e50e3cfd9469f2b
Philippe Timothe <PhilippeTimothe@gmail.com>
2021-03-25 14:52:16 -0400

    For loop Lesson Completed

--- 17-all-playgrounds-for-part-3/begin/Part 3 - (ALL EPISODES) Control Flow - Begin.playground/Pages/04 - For Loops.xcplaygroundpage/Contents.swift
+++ 17-all-playgrounds-for-part-3/begin/Part 3 - (ALL EPISODES) Control Flow - Begin.playground/Pages/04 - For Loops.xcplaygroundpage/Contents.swift
@@ -1,5 +1,34 @@
 //: [⇐ Previous: 03 - Challenge - While Loops](@previous)
 //: ## Episode 04: For Loops
+var usefulValue = 5
+let closedRange = 0...usefulValue
+let halfOpenRange = 0..<usefulValue
+
+var sum = 0
+let count = 10
+for i in 1...count {
+  sum += 1
+}
+print(sum)
+
+// or dont create a constant i by using the underscore(_).  Means there is not care for an index.
+
+for _ in 1...count {
+  print("Okay!")
+}
+
+// add a conditional with the "where" clause
+
+for _ in 1...count where count < 100 {
+  print("Okay!")
+}
+
+// find the odd with the modulo or mod (%)
+
+for i in 1...count where i % 2 == 1 {
+  print("\(i) is Odd!")
+}
+
 
 
 //: [⇒ Next: 05 - Challenge - For Loops](@next)
