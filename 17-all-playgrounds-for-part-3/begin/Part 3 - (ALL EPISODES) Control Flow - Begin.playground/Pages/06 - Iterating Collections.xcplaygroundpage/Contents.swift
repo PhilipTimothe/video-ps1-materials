06da2c9cdfba8d41b4c7f3671574ede7d54fae11
Philippe Timothe <PhilippeTimothe@gmail.com>
2021-03-26 11:57:17 -0400

    Iteration Lesson completed!

--- 17-all-playgrounds-for-part-3/begin/Part 3 - (ALL EPISODES) Control Flow - Begin.playground/Pages/06 - Iterating Collections.xcplaygroundpage/Contents.swift
+++ 17-all-playgrounds-for-part-3/begin/Part 3 - (ALL EPISODES) Control Flow - Begin.playground/Pages/06 - Iterating Collections.xcplaygroundpage/Contents.swift
@@ -5,5 +5,20 @@ let daysOfTheWeek: [String] = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thur
 
 let poolTemperature: [Int] = [78, 81, 74, 80, 79, 83, 84]
 
+//for i in 0..<daysOfTheWeek.count {
+//  print("\(daysOfTheWeek[i]): \(poolTemperature[i])" )
+//}
 
+for i in 0..<daysOfTheWeek.count where poolTemperature[i] >= 80 {
+  print("\(daysOfTheWeek[i]): \(poolTemperature[i])")
+}
+
+
+// Finding the average of pool temerature.
+var sum = 0
+for temperature in poolTemperature {
+  sum += temperature
+  print(sum)
+}
+sum / poolTemperature.count
 //: [⇒ Next: 07 - Challenge - Iterating Collections](@next)
