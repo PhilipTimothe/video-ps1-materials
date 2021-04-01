890264df53c179ea4482d1c68656cad0443a3648
Philippe Timothe <PhilippeTimothe@gmail.com>
2021-04-01 12:03:57 -0400

    Lesson Completed = Nested and break loop

--- 17-all-playgrounds-for-part-3/begin/Part 3 - (ALL EPISODES) Control Flow - Begin.playground/Pages/08 - Nested Loops and Early Exit.xcplaygroundpage/Contents.swift
+++ 17-all-playgrounds-for-part-3/begin/Part 3 - (ALL EPISODES) Control Flow - Begin.playground/Pages/08 - Nested Loops and Early Exit.xcplaygroundpage/Contents.swift
@@ -6,5 +6,38 @@ let daysOfTheWeek: [String] = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thur
 let poolTemperature: [Int] = [78, 81, 74, 80, 79, 83, 84]
 
 
+for i in 0..<daysOfTheWeek.count {
+  if daysOfTheWeek[i] == "Thursday" {
+    break
+  }
+  print("\(daysOfTheWeek[i]): \(poolTemperature[i])")
+}
 
+print("...............................")
+
+for i in 0..<daysOfTheWeek.count {
+  if daysOfTheWeek[i] == "Friday" {
+    print("I'm in love!")
+    continue
+  }
+  print("\(daysOfTheWeek[i]): \(poolTemperature[i])")
+}
+
+print("...............................")
+
+// Nested Loops
+
+floorLabel: for floor in 10...15 {
+//  add labels to loops
+  roomLabel: for room in 1...5 {
+    if room == 1 {
+      continue
+    }
+//    stop loop at 12th floor and room 3 and continue the main loop
+    if floor == 12 && room == 3 {
+      continue floorLabel
+    }
+    print("\(floor): \(room)")
+  }
+}
 //: [⇒ Next: 09 - Challenge - Nested Loops and Early Exit](@next)
