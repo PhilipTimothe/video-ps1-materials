55f392aaf1a7fb7437184146d322961f38d38e18
Philippe Timothe <PhilippeTimothe@gmail.com>
2021-04-02 12:53:18 -0400

    Reviewed important tidbits about the Dictionaries in swift.  Moving on to a code challenge.

--- 27-all-playgrounds-for-part-4/begin/Part 4 - (ALL EPISODES) More Collections - Begin.playground/Pages/02 - Creating and Populating Dictionaries.xcplaygroundpage/Contents.swift
+++ 27-all-playgrounds-for-part-4/begin/Part 4 - (ALL EPISODES) More Collections - Begin.playground/Pages/02 - Creating and Populating Dictionaries.xcplaygroundpage/Contents.swift
@@ -1,5 +1,15 @@
 //: [⇐ Previous: 01 - Introduction](@previous)
 //: ## Episode 02: Creating and Populating Dictionaries
+var emptyDictionary: [String: Int] = [:]
+
+var nameAndPets = ["Ron": "Rat",
+                   "Dan": "Dog",
+                   "Barry": "Bird"]
+print(nameAndPets)
+
+nameAndPets.updateValue("Cat", forKey: "Chris")
+nameAndPets["Bobby"] = "Bird"
 
 
+print(nameAndPets)
 //: [⇒ Next: 03 - Accessing and Working with Dictionaries](@next)
--- 27-all-playgrounds-for-part-4/begin/Part 4 - (ALL EPISODES) More Collections - Begin.playground/Pages/03 - Accessing and Working with Dictionaries.xcplaygroundpage/Contents.swift
+++ 27-all-playgrounds-for-part-4/begin/Part 4 - (ALL EPISODES) More Collections - Begin.playground/Pages/03 - Accessing and Working with Dictionaries.xcplaygroundpage/Contents.swift
@@ -11,6 +11,27 @@ var namesAndPets = [
     "Hiccup" : "🐉 Toothless"
 ]
 
+namesAndPets["Ron"]
+// ?? = default value
+let captainCrunch = namesAndPets["Capain Crucnch"] ?? "There is nobody with that name!"
+namesAndPets.isEmpty
+namesAndPets.count
 
+// cannot have a key without a value
+namesAndPets.removeValue(forKey: "Calvin")
+namesAndPets["Hiccup"] = nil
+print(namesAndPets)
 
+// looping dictionaries
+for (character, pet) in namesAndPets {
+  print("\(character) own's a \(pet)")
+}
+
+// iterate just keys or just values
+for name in namesAndPets.keys {
+  print(name)
+}
+for pets in namesAndPets.values {
+  print(pets)
+}
 //: [⇒ Next: 04 - Challenge - Dictionaries](@next)
