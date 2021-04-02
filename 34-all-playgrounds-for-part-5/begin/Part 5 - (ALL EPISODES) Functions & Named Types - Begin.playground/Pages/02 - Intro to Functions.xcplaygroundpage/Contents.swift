9b38a68f1c1eaf54f31fd6f37f38deb7f2dc8cd2
Philippe Timothe <PhilippeTimothe@gmail.com>
2021-04-02 19:26:45 -0400

    Swift Function lesson which introduced the underscore usage, along with external parameter naming etc.

--- 34-all-playgrounds-for-part-5/begin/Part 5 - (ALL EPISODES) Functions & Named Types - Begin.playground/Pages/02 - Intro to Functions.xcplaygroundpage/Contents.swift
+++ 34-all-playgrounds-for-part-5/begin/Part 5 - (ALL EPISODES) Functions & Named Types - Begin.playground/Pages/02 - Intro to Functions.xcplaygroundpage/Contents.swift
@@ -1,6 +1,38 @@
 //: [⇐ Previous: 01 - Introduction](@previous)
 //: ## Episode 02: Introduction to Functions
 
+func printHello() {
+  print("Hello")
+}
 
+//printHello()
+
+// --------------------------------------
+let passingGrade = 50
+let chrisGrade = 49
+let samGrade = 99
+
+let chrisPassed = chrisGrade >= passingGrade
+let samPassed = samGrade >= passingGrade
+// --------------------------------------
+
+func printPassingStatus(for grade: Int, lowestGrade: Int = passingGrade) {
+  print(grade >= passingGrade ? "You Passed" : "Study More!")
+}
+
+//printPassingStatus(grade: samGrade)
+
+//external parameter name
+printPassingStatus(for: samGrade)
+
+
+
+// -------------------------------------- Add underscore to parameter so arguments dont have to us a label
+
+func printHighestGrade(_ grade1: Int, _ grade2: Int) {
+  print(grade1 >= grade2 ? grade1 : grade2)
+}
+
+printHighestGrade(chrisGrade, samGrade)
 
 //: [⇒ Next: 03 - Functions and Return](@next)
